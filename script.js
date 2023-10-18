1f6a6a5f65ee655ab8ed1ea6d2da6fa194cb572c
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-18 19:16:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//your code here
+function nonArticleNumber(input){
+	let word = input.split();
+	let nonArticleWord = filter.map((word) => {
+		let lowerCaseWord = word.toLowerCase();
+		if(lowerCaseWord==="the"||lowerCaseWord==="an"||lowerVaseWord==="a")
+			return false;
+	}
+		return	true;						
+});
+
+  return  nonArticleWord.join(" ");
+
 
