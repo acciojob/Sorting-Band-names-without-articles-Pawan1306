dc116be764bc903bc68a625ab175abb495f99a76
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-21 08:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-test cases fails function ArticleLess(input) {
-  let words = input.split(" ");
-  let nonArticleWords = words.filter((word) => {
-    let lowerCaseWord = word.toLowerCase();
-    return !(lowerCaseWord === "the" || lowerCaseWord === "an" || lowerCaseWord === "a");
-  });
+function sortBandNames(names) {
+  return names.sort((a, b) => {
+    // Helper function to remove articles and leading/trailing spaces
+    const removeArticles = (name) => name.replace(/^(a|an|the)\s+/i, '').trim();
+    
+    // Compare band names without articles
+    const nameA = removeArticles(a).toLowerCase();
+    const nameB = removeArticles(b).toLowerCase();
 
-  return nonArticleWords.join(" ");
+    return nameA.localeCompare(nameB);
+  });
 }
+
+// Sort the band names and create the HTML list
+function createBandList(names) {
+  const sortedBandNames = sortBandNames(names);
+  const bandList = document.getElementById('band');
+
+  sortedBandNames.forEach((name) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = name;
+    bandList.appendChild(listItem);
+  });
+}
+
+// Call the function to create the band list
+createBandList(bandNames);
 
